e1388a2bf0ebd16355da6c26915f6bfdfcd4c41d
Odrec <renatogarita@gmail.com>
2016-10-01 14:50:24 +0200

    first version of NN

---
+++ simple_neural_network.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  1 12:09:06 2016
+
+Simple neural net for the prototype
+
+@author: Renato Garita Figueiredo
+"""
+
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+import keras
+
+class NN:
+    
+    def __init__(self):
+        self.model = Sequential()
+         
+    #@params num_input_nodes Number of input nodes. In this case the amount of
+    #modules for feature extraction
+    def initializeNN(self, num_input_nodes):
+        
+        print("Creating model...")
+        
+        #This is a well-known heuristic where the number of nodes 
+        #on the hidden layer is the mean of the input and output layers
+        hidden=np.mean([num_input_nodes,1]).astype(int)
+        
+        #Input and hidden layers
+        self.model.add(Dense(output_dim=hidden, input_dim=num_input_nodes),
+                       activation="sigmoid", init="uniform")
+                       
+        #Output layer
+        #Activation function Sigmoid
+        self.model.add(Dense(output_dim=1), activation="sigmoid")
+        
+        #Compile model
+        #The loss function is binary_crossentropy since we are dealing with 
+        #just two classes. Stochastic gradient descent
+        self.model.compile(loss="binary_crossentropy", optimizer="sgd", metrics=["accuracy"])
+        
+        
+    #@params train_data a list of numpy arrays. Each array is an input
+    #@params train_labels a numpy array with the target data
+    def trainNN(self, train_data, train_labels):
+        print("Training model...")
+        self.model.fit(train_data, train_labels, nb_epoch=50, batch_size=100)
+        self.model.save("NN.model")
+        
+    #@params test_data a list of numpy arrays. Each array is an input
+    #@params test_labels a numpy array with the target data
+    def testNN(self, test_data, test_labels):
+        print("Testing model...")
+        self.model=keras.models.load_model("NN.model")
+        (loss,accuracy)=self.model.evaluate(test_data, test_labels, nb_epoch=50, batch_size=100)
+        print("loss={:.4f}, accuracy: {:.4f}%".format(loss, accuracy * 100))
+        
+        
