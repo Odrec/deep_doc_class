abf57e129cd76f65ea3ae5293aa1bacabb48120a
Odrec <renatogarita@gmail.com>
2016-10-24 12:10:21 +0200

    latest Master.py 24 oct

--- Master.py
+++ Master.py
@@ -17,7 +17,8 @@ from Text_Score_Module import TextScore
 from bow_pdf_test import BoW_Text_Module
 from page_size_ratio_module import Page_Size_Module
 from scanner_detect_module import ScannerDetect
-from naive_scan_detector import Naive_Scan_Detector
+from bow_metadata import Bow_Metadata
+from orientation_detector import page_orientation_module
 
 #from ocr_bow_module import OCR_BoW_Module
 #INSERT IMPORT OF NEURAL NETWORK HERE
@@ -60,10 +61,9 @@ def get_data_vector(modules, filepointer, metapointer=None):
             #extract data-dimension from pdf
             data.append(m.get_function(filepointer,metapointer))
         except:
-            #if error occures the value is 0.0
-            data.append(0.0)
-    #return as numpy array
-    #return np.array(data)
+            #if error occures the value is nan
+            data.append(np.nan)
+
     return data
 
 #extracts features from a set of files and returns them on a list of lists
@@ -122,79 +122,39 @@ def get_classes(filenames,classes,metadata):
         
 #loads the features from the csv file created during extraction
 #
-#@result:                 list of lists of features, list of corresponding classes and filenames
-def load_data():
+#@result:   list of lists of features, list of corresponding classes and filenames
+def load_data(features_file):
     
-    with open('output.csv', 'r') as f:
+    with open(features_file, 'r') as f:
       reader = csv.reader(f)
       data = list(reader)
       
     num_features = len(data[0])-2
             
     features = [item[:num_features] for item in data]
+
+    features = generate_error_features(features)     
+    
     classes = [item[num_features] for item in data]
     filenames = [item[num_features+1] for item in data]
     
     return features, classes, filenames
     
-#merges the features from two csv files created during extraction
+#generates the error features and replaces the nan values
 #
-#@param file1/file2:    files to merge
-#@result:               new file with features merged
-def merge_features(file1,file2):
+#@result:   list of features with the error features included
+def generate_error_features(features):
     
-    with open(file1, 'r') as f:
-      reader1 = csv.reader(f)
-      data1 = list(reader1)
-      
-    with open(file2, 'r') as f:
-      reader2 = csv.reader(f)
-      data2 = list(reader2)
-      
-    num_files1 = len(data1)
-    num_files2 = len(data2)
+    error_feature = [0.0] * len(features)
     
-    if num_files1 == num_files2:
-        
-        len1 = len(data1[0])
-        len2 = len(data2[0])
-        
-        data1.sort(key=lambda x: x[len1-1])
-        data2.sort(key=lambda x: x[len2-1])
+    for i, x in enumerate(features):
+        for j, e in enumerate(x):
+            if e == 'nan':
+                error_feature[i] = 1.0
+                x[j] = 1.0                
     
-        filenames1 = [item[len1-1] for item in data1]
-        filenames2 = [item[len2-1] for item in data2]
-    
-        if filenames1 == filenames2:
-            
-            num_features1 = len1-2
-            num_features2 = len2-2
-            
-            features1 = [item[:num_features1] for item in data1]
-            classes = [item[num_features1] for item in data1]
-            
-            features2 = [item[:num_features2] for item in data2]
-                                    
-            features=[x + features2[i] for i, x in enumerate(features1)]
-            
-            data=[features[i]+[x] for i, x in enumerate(classes)]
-            
-            data=[data[i]+[x] for i, x in enumerate(filenames1)]
-
-            with open("output_new.csv","w") as f:
-                writer = csv.writer(f)
-                writer.writerows(data)
-
-        else:
-            print("ERROR: Both files should have features for the same list of entries")
-            return False
-        
-    else:
-        print("ERROR: Both files to merge should contain features for the same amount of entries")
-        return False
-        
-    return True
-    
+    #features = [x + [error_feature[i]] for i, x in enumerate(features)]
+    return features
     
     
 #function to train modules if needed. Each module called should have a train function
@@ -234,16 +194,25 @@ def getNN(input_dim):
     return network
 
 args = sys.argv
+len_args = len(args)
 training = False
-extracting = True
+extracting = False
 #train mode
 if '-t' in args:
     training = True
     TESTSIZE = 1000#should we use a percentage of the total data instead? Ex. 80%
+    if len_args == 3:
+        features_file = args[2]
+    elif len_args == 4:
+        features_file = args[3]
+        
+    if not os.path.isfile(features_file):
+        print("Error: Features file doesn't exist.")
+        exit();
     
 if '-e' in args:
     extracting= True
-    TESTSIZE = 1000#should we use a percentage of the total data instead? Ex. 80%    
+    TESTSIZE = 1000#should we use a percentage of the total data instead? Ex. 80%     
 
 #init filepointer for save-file here, the file will contain all classifications
 #maybe csv-file, not yet decided
@@ -251,7 +220,7 @@ if '-e' in args:
 if(not os.path.isfile('classes.csv')):
     with open('classes.csv', 'w') as outcsv:
         writer = csv.writer(outcsv,delimiter=';')
-        writer.writerow(["Filename", "TextScore", "BoW_Text", "Page_Size_Module", "ScannerDetect", "Classification"])
+        writer.writerow(["Filename", "NaiveScan", "Classification"])
 #open file for pruning
 save_file = open('classes.csv','r')
 
@@ -260,16 +229,21 @@ conf_thresh = 0.5
 
 #initialize module
 modules = list()
-modules.append(TextScore(True))
-modules.append(BoW_Text_Module(True))
-modules.append(Page_Size_Module())
-modules.append(ScannerDetect())
-#modules.append(Naive_Scan_Detector())
-#modules.append(OCR_BoW_Module())
-#ADD MODULES HERE
 
-#init neural network
-network = getNN(len(modules))
+#ADD MODULES HERE
+#modules.append(TextScore(True))
+#modules.append(BoW_Text_Module(True))
+#modules.append(Page_Size_Module())
+#modules.append(ScannerDetect())
+#modules.append(page_orientation_module())
+#modules.append(Bow_Metadata("title"))
+modules.append(Bow_Metadata("author"))
+#modules.append(Bow_Metadata("filename"))
+#modules.append(Bow_Metadata("folder_name"))
+#modules.append(Bow_Metadata("folder_description"))
+#modules.append(Bow_Metadata("description"))
+
+#modules.append(OCR_BoW_Module())
 
 #get filenames
 path = './files'
@@ -288,38 +262,32 @@ save_file = open('classes.csv','a')
 if(extracting):
     print("Extracting Features from the training set. This will take a while...")
     extract_features(filenames,classes,metadata)
-    
 
 #START TRAINING HERE
 if(training):
-    
-    #train, filenames = MetaHandler.gen_train_test_split(filenames,TESTSIZE)
-        
-    features,classes,files = load_data()
+            
+    features, classes, files = load_data(features_file)
         
     features = [[float(j) for j in i] for i in features]
     
     classes = [float(i) for i in classes]
     
-    max_nor=max(map(lambda x: x[2], features))
-    max_nor2=max(map(lambda x: x[1], features))
+    len_feat = len(features[0])
     
-    min_nor=min(map(lambda x: x[2], features))
-    min_nor2=min(map(lambda x: x[1], features))
-
-#    for f in features: f[2] /= max_nor
-#        
-#    for f in features: f[1] /= max_nor2
-    
-    for f in features: (f[2] - min_nor)/(max_nor-min_nor)
-        
-    for f in features: (f[1] - min_nor2)/(max_nor2-min_nor2)
+    for i in range(0, len_feat):
+        max_nor=max(map(lambda x: x[i], features))
+        min_nor=min(map(lambda x: x[i], features))
+        for f in features: (f[i] - min_nor)/(max_nor-min_nor)
     
     features=np.array([np.array(xi) for xi in features])
     
+    print("Initiating Neural Network")
+    network = getNN(len(features[0]))
+    print("Initialization finished")
+    
+    print("Starting training.")
     network.trainNN(features,np.array(classes))
     print("Training done!")
-
 
 
 #classification
