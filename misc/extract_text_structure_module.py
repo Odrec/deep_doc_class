052180d9da9625bc9d440a8fe6bde6946e26d29a
Odrec <renatogarita@gmail.com>
2017-03-16 22:09:02 +0100

    json data and added extraction of structure data online

--- misc/extract_text_structure_module.py
+++ misc/extract_text_structure_module.py
@@ -6,7 +6,8 @@ Created on Thu Oct 27 14:13:00 2016
 """
 
 import sys, os
-import csv
+import csv, json
+from multiprocessing import Pool
 
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
@@ -18,86 +19,34 @@ from pdfminer.layout import LAParams, LTTextBox, LTTextLine, LTFigure, LTImage
 from os.path import basename, join, splitext, isfile, isdir
 from doc_globals import*
 
-def parse_layout(filename, page, lt_obj):
+def parse_layout(filename, dict_structure, page, lt_obj, num_obj, num_subobj=0):
     
     """Function to recursively parse the layout tree."""
-    sublist = []
-
-    if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
-        sublist.append(filename)
-        sublist.append(page)
-        sublist.append(lt_obj.__class__.__name__)
-        sublist.append(lt_obj.bbox[0])
-        sublist.append(lt_obj.bbox[1])
-        sublist.append(lt_obj.bbox[2])
-        sublist.append(lt_obj.bbox[3])
-        sublist.append(lt_obj.get_text())
-
-#        print(lt_obj.__class__.__name__)            
-#        print(lt_obj.bbox)
-#        print(lt_obj.get_text())
-    elif isinstance(lt_obj, LTFigure):
-        for lt_obj_int in lt_obj:
-            return parse_layout(filename, page, lt_obj_int)
-        return 0
-#        print(lt_obj.__class__.__name__)            
-#        parse_layout(filename, page, lt_obj)  # Recursive
-    elif isinstance(lt_obj, LTImage):
-        sublist.append(filename)
-        sublist.append(page)
-        sublist.append(lt_obj.__class__.__name__)
-        sublist.append(lt_obj.bbox[0])
-        sublist.append(lt_obj.bbox[1])
-        sublist.append(lt_obj.bbox[2])
-        sublist.append(lt_obj.bbox[3])
-            
-    return sublist
-
-
-args = sys.argv
-usage = "Usage: python extract_text_structure_module.py file|path_to_files|file_list_csv"
-if len(args) != 2:
-    print(usage)
-    sys.exit(1)
     
-f = args[1]
-names_list = []
-files = []
-if isfile(f):
-    ext = f[-3:]
-    if ext == 'csv':
-        with open(f, "r") as fl:
-            reader = list(csv.reader(fl, delimiter=","))
-            reader = reader[0]
-            for i, line in enumerate(reader):
-                for root, dirs, fls in os.walk(PDF_PATH):
-                    for name in fls:
-                        if name == line:
-                            if not name in names_list:
-                                files.append(join(root,name))
-                                names_list.append(name)
-    elif ext == 'pdf':
-        files.append(f)
-elif isdir(f):
-    for root, dirs, fls in os.walk(f):
-        for name in fls:
-            if splitext(basename(name))[1] == '.pdf':
-                files.append(join(root,name))
-else:
-    print("Error: You need to specify a pdf file or path or csv file with the list of files.")
-    print(usage)
-    sys.exit(1)
-        
-files_structure = []
-print(len(files)) 
-for i in files:
-    print(basename(i))
-    filename = splitext(basename(i))[0]
-    fp = open(i, 'rb')
+    if isinstance(lt_obj, LTFigure):
+        num_subobj = 0
+        for lt_obj_int in lt_obj:
+            parse_layout(filename, dict_structure, page, lt_obj_int, num_obj, num_subobj)
+            num_subobj += 1
+    else:
+        if isinstance(lt_obj, LTImage) or isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+            if not page in dict_structure[filename]:
+                dict_structure[filename][page] = {}
+            dict_structure[filename][page][lt_obj.__class__.__name__+'-'+str(num_obj)+'-'+str(num_subobj)] = {}
+            dict_structure[filename][page][lt_obj.__class__.__name__+'-'+str(num_obj)+'-'+str(num_subobj)].update({'box1': lt_obj.bbox[0],
+            'box2': lt_obj.bbox[1],'box3': lt_obj.bbox[2],'box4': lt_obj.bbox[3]})
+                
+            if not isinstance(lt_obj, LTImage):
+                dict_structure[filename][page][lt_obj.__class__.__name__+'-'+str(num_obj)+'-'+str(num_subobj)].update({'text':lt_obj.get_text()})
+                
+def process_file(file):
+    filename = splitext(basename(file))[0]
+    fp = open(file, 'rb')
+    dict_structure = {}
+    dict_structure[filename] = {}
     try:
         parser = PDFParser(fp)
         doc = PDFDocument(parser)
-        
         rsrcmgr = PDFResourceManager()
         laparams = LAParams()
         device = PDFPageAggregator(rsrcmgr, laparams=laparams)
@@ -107,15 +56,68 @@ for i in files:
             num_page += 1
             interpreter.process_page(page)
             layout = device.get_result()
+            num_obj = 0
             for lt_obj in layout:
-                res = parse_layout(filename, num_page, lt_obj)
-                if res != 0:
-                    files_structure.append(res)
-            #if num_page == 1 break #control here how many pages you want to process per file
+                parse_layout(filename, dict_structure, num_page, lt_obj, num_obj)
+                num_obj += 1
     except:
-        print("Failed to extract structure from file ",basename(i))
+        print("Failed to extract structure from file ",basename(file))
         pass
+
+    return dict_structure
+
+if __name__ == "__main__":
+
+    args = sys.argv
+    usage = "Usage: python extract_text_structure_module.py file|path_to_files|file_list_csv [-c number_of_cores]"
+    largs = len(args)
+    if largs >= 2:
+        f = args[1]
+        names_list = []
+        files = []
+        if isfile(f):
+            ext = f[-3:]
+            if ext == 'csv':
+                with open(f, "r") as fl:
+                    reader = list(csv.reader(fl, delimiter=","))
+                    reader = reader[0]
+                    for i, line in enumerate(reader):
+                        for root, dirs, fls in os.walk(PDF_PATH):
+                            for name in fls:
+                                if name == line:
+                                    if not name in names_list:
+                                        files.append(join(root,name))
+                                        names_list.append(name)
+            elif ext == 'pdf':
+                files.append(f)
+        elif isdir(f):
+            for root, dirs, fls in os.walk(f):
+                for name in fls:
+                    if splitext(basename(name))[1] == '.pdf':
+                        files.append(join(root,name))
+        else:
+            print("Error: You need to specify a pdf file or path or csv file with the list of files.")
+            print(usage)
+            sys.exit(1)
             
-with open("output_text_structure.csv", "w") as f:
-    writer = csv.writer(f)
-    writer.writerows(files_structure)
+        if largs == 4:
+            num_cores = int(args[4])
+        else:
+            num_cores = 1
+    
+    else:
+        print(usage)
+        sys.exit(1)    
+    
+            
+    print(len(files))
+    
+    pool = Pool(num_cores)
+    res = pool.map(process_file, files)
+    res_fix={}
+    for x in res:    
+        res_fix.update(x)
+    
+    
+    with open("output_text_structure.json", 'w') as fp:
+        json.dump(res_fix, fp)
--- misc/structure_info_module.py
+++ misc/structure_info_module.py
@@ -6,54 +6,71 @@ Created on Sat Feb 11 10:37:29 2017
 @author: odrec
 """
 
-import sys, csv
+import sys, os, json, codecs
 import numpy as np
 from os.path import basename, join, splitext, isfile, isdir
+import extract_text_structure_module as es
 
 class Structure_Info:
     
-    def __init__(self): 
+    def __init__(self, path=None): 
+        
+        if path != None:
+            file = path
+        else:
+            file = 'output_text_structure.json'
+            
+        self.struct_elements = self.get_struct_elements(file)
         return None
 
     #@return ratio of text boxes to image boxes, ratio of text boxes per page, ratio of words per text box, mean area of text boxes
     #ratio of image boxes per page, mean area of image boxes    
-    def get_function(self, filename, metapointer = None):
+    def get_function(self, file, metapointer = None):
         
+        file_key = splitext(basename(file))[0]
+        if file_key in self.struct_elements:
+            struct_data = self.struct_elements[file_key]
+        else:
+            struct_data = es.process_file(file)
+            struct_data = struct_data[file_key]
+            
+        names = ['ratio_text_image', 'ration_text_pages', 'ratio_words_box', 'avg_text_size', 'ratio_image_pages', 'avg_image_size']
         try:
-            elements_list = self.get_struct_elements(filename)
-            num_pages = self.get_num_pages(elements_list)
-            textbox_list = self.get_textboxes(elements_list)
-            imagebox_list = self.get_imageboxes(elements_list)
+            num_pages = self.get_num_pages(struct_data)
+            textbox_list, imagebox_list = self.get_boxes(struct_data)
             ntb, txtinfo = self.get_textbox_info(textbox_list, num_pages)
             nib, imginfo = self.get_imagebox_info(imagebox_list, num_pages)
             if nib != 0:
                 ratio_tb_ib = ntb/nib
             else:
                 ratio_tb_ib = 0
-            return ratio_tb_ib, txtinfo, imginfo
+            
+            return names, [ratio_tb_ib]+txtinfo+imginfo
         except:
-            return [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
+            return names, [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
 
     def get_struct_elements(self, filename):
-        file_elements = []
-        with open("output_text_structure.csv", "r") as f:
-            reader = csv.reader(f)
-            for i, r in enumerate(reader):
-                if len(r) > 0:
-                    if r[0] == filename:
-                        file_elements.append(r)
+        file_elements = {}
+        if isfile(filename):
+            with codecs.open(filename, 'r', encoding='utf-8', errors='replace') as j:
+                json_data=j.read()
+                file_elements = json.loads(json_data)
             
         return file_elements
         
-    def get_num_pages(self, elements_list):
-        return max([int(e[1]) for e in elements_list])
+    def get_num_pages(self, struct_data):
+        return len(struct_data)
         
-    def get_textboxes(self, elements_list):
+    def get_boxes(self, struct_data):
         tb_result = []
-        for e in elements_list:
-            if "Text" in e[2]:
-                tb_result.append(e)
-        return tb_result
+        ib_result = []
+        for k in struct_data:
+            for kk in struct_data[k]:
+                if "Text" in kk:
+                    tb_result.append(struct_data[k][kk])
+                elif "Image" in kk:
+                    ib_result.append(struct_data[k][kk])
+        return tb_result, ib_result
         
     def get_textbox_info(self, textbox_list, num_pages):
         num_textboxes = len(textbox_list)
@@ -67,23 +84,16 @@ class Structure_Info:
     def get_num_words(self, textbox_list):
         num_words = 0
         for e in textbox_list:
-            num_words += len(e[7].split())
+            num_words += len(e['text'].split())
         return num_words
         
     def get_tb_size_avg(self, textbox_list):
         area = []
         for e in textbox_list:
-            xs = float(e[5])-float(e[3])
-            ys = float(e[6])-float(e[4])
+            xs = float(e['box3'])-float(e['box1'])
+            ys = float(e['box4'])-float(e['box2'])
             area.append(xs*ys)
         return np.mean(area)
-        
-    def get_imageboxes(self, elements_list):
-        ib_result = []
-        for e in elements_list:
-            if "Image" in e[2]:
-                ib_result.append(e)
-        return ib_result
         
     def get_imagebox_info(self, imagebox_list, num_pages):
         num_imageboxes = len(imagebox_list)
@@ -95,18 +105,40 @@ class Structure_Info:
     def get_ib_size_avg(self, imagebox_list):
         area = []
         for e in imagebox_list:
-            xs = float(e[5])-float(e[3])
-            ys = float(e[6])-float(e[4])
+            xs = float(e['box3'])-float(e['box1'])
+            ys = float(e['box4'])-float(e['box2'])
             area.append(xs*ys)
         return np.mean(area)
         
     
 if __name__ == "__main__":
     
-    SI = Structure_Info()
-
     args = sys.argv
     
-    filename = splitext(basename(args[1]))[0]
+    usage = "python structure_info_module.py [json_file] path_pdf_file(s)"
+            
+    if len(args)>2:
+        if not isfile(args[1]):
+            print(usage)
+            sys.exit(1)
+        SI = Structure_Info(args[1])
+        path_pdf = args[2]
+    else:
+        SI = Structure_Info()
+        path_pdf = args[1]
 
-    print(SI.get_function(filename))
+    files = []
+    if isdir(path_pdf):
+        for root, dirs, fls in os.walk(path_pdf):
+            for name in fls:
+                if splitext(basename(name))[1] == '.pdf':
+                    files.append(join(root,name))
+    else:
+        files = [path_pdf]
+    results=[]
+    for f in files:
+        results.append(SI.get_function(f))
+        
+    print(results)
+        
+    
